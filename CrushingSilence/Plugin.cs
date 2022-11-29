aa4dddf28dcffa494bc9a9218dee66e35889e68c
erkle64 <erkle64@gmail.com>
2022-11-30 12:30:38 +1300

    Update Plugin.cs

--- CrushingSilence/Plugin.cs
+++ CrushingSilence/Plugin.cs
@@ -48,29 +48,6 @@ namespace CrushingSilence
                     __instance.producer_audioClip_active = null;
                 }
             }
-
-            //private static bool silenceCurrentMachine = false;
-
-            //[HarmonyPatch(typeof(CrusherGO), nameof(CrusherGO.machineUpdates))]
-            //[HarmonyPrefix]
-            //private static void CrusherGO_machineUpdates(CrusherGO __instance)
-            //{
-            //    silenceCurrentMachine = true;
-            //}
-
-            //[HarmonyPatch(typeof(CrusherGO), nameof(CrusherGO.machineUpdates))]
-            //[HarmonyPostfix]
-            //private static void CrusherGO_machineUpdatesPostfix(CrusherGO __instance)
-            //{
-            //    silenceCurrentMachine = false;
-            //}
-
-            //[HarmonyPatch(typeof(AudioSourceFader), nameof(AudioSourceFader.renderUpdate), new Type[] { typeof(bool), typeof(bool) })]
-            //[HarmonyPrefix]
-            //private static void AudioSourceFader_renderUpdate(AudioSourceFader __instance, ref bool state, ref bool enableSound)
-            //{
-            //    if (silenceCurrentMachine) enableSound = false;
-            //}
         }
     }
 }
